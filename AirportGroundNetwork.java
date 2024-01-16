f50ccc727d4aea5716ef6dabda641d0d7079888f
Hashir-Ashraf-Awan <142582802+Hashir-Ashraf-Awan@users.noreply.github.com>
2024-01-16 15:28:53 +0500

    Update AirportGroundNetwork.java

--- AirportGroundNetwork.java
+++ AirportGroundNetwork.java
@@ -2,93 +2,143 @@ package task;
 
 import java.util.*;
 
-class AirportGroundNetwork {
+public class AirportGroundNetwork {
 
-    private Map<String, LinkState> runwayStates;
-
-    private Map<String,LinkState> gatestates;
-    private Map<String,LinkState>taxiwaystates;
-    private Map<String,Integer> LinkOccupancy;
+    protected Map<String, LinkState> runwayStates;
+    protected Map<String, LinkState> gatestates;
+    protected Map<String, LinkState> taxiwaystates;
+    protected Map<String, Integer> LinkOccupancy;
 
     public AirportGroundNetwork() {
-
-        this.runwayStates=new HashMap<>();
-        this.taxiwaystates=new HashMap<>();
-
+        this.runwayStates = new HashMap<>();
+        this.gatestates = new HashMap<>();
+        this.taxiwaystates = new HashMap<>();
     }
 
     // Enum to represent the states of a link (runway, taxiway, or gate)
     private enum LinkState {
-        OPEN_AVAILABLE, OPEN_OCCUPIED,CLOSED
+        OPEN_AVAILABLE, OPEN_OCCUPIED, CLOSED
     }
 
-    // Method to update link state when it is opened
-    public void openLink(String linkName) {
-        if (Objects.equals(linkName, "Gate")) {
-            gatestates.put(linkName, LinkState.OPEN_AVAILABLE);
-        }
-        if (Objects.equals(linkName, "Taxiway")){
-            taxiwaystates.put(linkName, LinkState.OPEN_AVAILABLE);}
-        if (Objects.equals(linkName, "Runway")){
-            runwayStates.put(linkName, LinkState.OPEN_AVAILABLE);}
-
-        System.out.println(linkName + " is now open and available.");
+    public void openGate(String linkName) {
+        gatestates.put(linkName, LinkState.OPEN_AVAILABLE);
+        System.out.println("Link " + linkName + " is now open and available.");
     }
 
-    // Method to update link state when it is occupied
-    public void occupyLink(String linkName) {
-        if (Objects.equals(linkName, "Gate")) {
+    public void openRunway(String linkName) {
+        runwayStates.put(linkName, LinkState.OPEN_AVAILABLE);
+        System.out.println("Link " + linkName + " is now open and available.");
+    }
+
+    public void openTaxiway(String linkName) {
+        taxiwaystates.put(linkName, LinkState.OPEN_AVAILABLE);
+        System.out.println("Link " + linkName + " is now open and available.");
+    }
+
+    public void occupyGate(String linkName) {
             gatestates.put(linkName, LinkState.OPEN_OCCUPIED);
-        }
-        if (Objects.equals(linkName, "Taxiway")){
-            taxiwaystates.put(linkName, LinkState.OPEN_OCCUPIED);}
-        if (Objects.equals(linkName, "Runway")){
-            runwayStates.put(linkName, LinkState.OPEN_OCCUPIED);}
+            System.out.println("Link " + linkName + " occupied.");
 
     }
 
-    // Method to update link state when it is closed
-    public void closeLink(String linkName) {
-        if (Objects.equals(linkName, "Gate")) {
-            gatestates.put(linkName, LinkState.CLOSED);
+    public void occupyRunway(String linkName) {
+
+            runwayStates.put(linkName, LinkState.OPEN_OCCUPIED);
+            System.out.println("Link " + linkName + " occupied.");
         }
-        if (Objects.equals(linkName, "Taxiway")){
-            taxiwaystates.put(linkName, LinkState.CLOSED);}
-        if (Objects.equals(linkName, "Runway")){
-            runwayStates.put(linkName, LinkState.CLOSED);}
+
+
+    public void occupyTaxiway(String linkName) {
+
+            taxiwaystates.put(linkName, LinkState.OPEN_OCCUPIED);
+            System.out.println("Link " + linkName + " occupied.");
+
     }
 
-    // Method to update link occupancy when an airplane enters a link
-    public void airplaneEntersLink(String linkName, int airplaneID) {
+    public void closeGate(String linkName) {
+        gatestates.put(linkName, LinkState.CLOSED);
+        System.out.println("Link " + linkName + " is now closed.");
+    }
 
-        if (Objects.equals(linkName, "Gate")) {
+    public void closeRunway(String linkName) {
+        runwayStates.put(linkName, LinkState.CLOSED);
+        System.out.println("Link " + linkName + " is now closed.");
+    }
+
+    public void closeTaxiway(String linkName) {
+        taxiwaystates.put(linkName, LinkState.CLOSED);
+        System.out.println("Link " + linkName + " is now closed.");
+    }
+
+    public void airplaneEntersGate(String linkName, int airplaneID) {
+      
             gatestates.put(linkName, LinkState.OPEN_OCCUPIED);
             LinkOccupancy.put(linkName, airplaneID);
+            System.out.println("Airplane " + airplaneID + " entered " + linkName);
         }
-        if (Objects.equals(linkName, "Taxiway")) {
+    public void airplaneEntersRunway(String linkName, int airplaneID) {
+     
             taxiwaystates.put(linkName, LinkState.OPEN_OCCUPIED);
             LinkOccupancy.put(linkName, airplaneID);
+            System.out.println("Airplane " + airplaneID + " entered " + linkName);
         }
-        if (Objects.equals(linkName, "Runway")) {
+    public void airplaneEntersTaxiway(String linkName, int airplaneID) {
             runwayStates.put(linkName, LinkState.OPEN_OCCUPIED);
             LinkOccupancy.put(linkName, airplaneID);
+            System.out.println("Airplane " + airplaneID + " entered " + linkName);
         }
+    
+
+    public void airplaneExitsGate(String linkName, int airplaneID) {
+       
+            gatestates.put(linkName, LinkState.OPEN_AVAILABLE);
+            LinkOccupancy.remove(linkName);
+            System.out.println("Airplane " + airplaneID + " exited " + linkName);
+        }
+    public void airplaneExitsTaxiway(String linkName, int airplaneID) {
+
+        taxiwaystates.put(linkName, LinkState.OPEN_AVAILABLE);
+            LinkOccupancy.remove(linkName);
+            System.out.println("Airplane " + airplaneID + " exited " + linkName);
+        }
+    public void airplaneExitsRunway(String linkName, int airplaneID) {
+
+      
+            runwayStates.put(linkName, LinkState.OPEN_AVAILABLE);
+            LinkOccupancy.remove(linkName);
+            System.out.println("Airplane " + airplaneID + " exited " + linkName);
+        
     }
 
-
-    // Method to update link occupancy when an airplane exits a link
-    public void airplaneExitsLink(String linkName, int airplaneID) {
-        if (Objects.equals(linkName, "Gate")) {
-            gatestates.put(linkName, LinkState.OPEN_AVAILABLE);
-            LinkOccupancy.remove(linkName, airplaneID);
-        }
-        if (Objects.equals(linkName, "Taxiway")) {
-            taxiwaystates.put(linkName, LinkState.OPEN_AVAILABLE);
-            LinkOccupancy.remove(linkName, airplaneID);
-        }
-        if (Objects.equals(linkName, "Runway")) {
-            runwayStates.put(linkName, LinkState.OPEN_AVAILABLE);
-            LinkOccupancy.remove(linkName, airplaneID);
+    public String getRunwayByAirplaneId(int airplaneId) {
+        // Logic to find the runway based on airplaneId
+        for (Map.Entry<String, LinkState> entry : runwayStates.entrySet()) {
+            if (entry.getValue() == LinkState.OPEN_OCCUPIED) {
+                return entry.getKey(); // Return the runway name
             }
         }
+        return null; // If no runway is found for the given airplaneId
     }
+
+    public String getTaxiwayByAirplaneId(int airplaneId) {
+        // Logic to find the taxiway based on airplaneId
+        for (Map.Entry<String, LinkState> entry : taxiwaystates.entrySet()) {
+            if (entry.getValue() == LinkState.OPEN_OCCUPIED) {
+                return entry.getKey(); // Return the taxiway name
+            }
+        }
+        return null; // If no taxiway is found for the given airplaneId
+    }
+
+    public String getGateByAirplaneId(int airplaneId) {
+        // Logic to find the gate based on airplaneId
+        for (Map.Entry<String, LinkState> entry : gatestates.entrySet()) {
+            if (entry.getValue() == LinkState.OPEN_OCCUPIED) {
+                return entry.getKey(); // Return the gate name
+            }
+        }
+        return null; // If no gate is found for the given airplaneId
+    }
+
+    // Add similar methods for other functionalities as needed
+}
